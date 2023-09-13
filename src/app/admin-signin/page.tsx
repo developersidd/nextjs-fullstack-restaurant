0fd5521a78232513e6d17493d809926fe87ea0a0
AB Siddik <siddik.prgmr@gmail.com>
2023-09-13 20:15:10 +0600

    changes

--- src/app/admin-signin/page.tsx
+++ src/app/admin-signin/page.tsx
@@ -1,108 +1,13 @@
-"use client";
-import loadingGear from "@/assets/images/loading-gear.gif";
-import warning from "@/assets/images/warning.png";
-import SocialMediaAuth from "@/components/SocialMediaAuth/SocialMediaAuth";
-import Input from "@/components/shared/Input/Input";
-import { useSigninMutation } from "@/redux/features/signin/signinApi";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Metadata } from "next";
-import Image from 'next/image';
-import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "react-hot-toast";
-import * as yup from "yup";
-const schema = yup.object().shape({
-    email: yup.string().email().required(),
-    password: yup.string().min(10).max(30).required(),
-});
+import AdminSignIn from '@/components/AdminSignIn/AdminSignIn'
+import { Metadata } from 'next'
+import React from 'react'
 
 export const metadata: Metadata = {
     title: 'Admin Sign In - Siddik Restaurant',
 }
 
-
 const AdminSignInPage = () => {
-    const [signIn, { isSuccess, data, isLoading, isError, error }] = useSigninMutation();
-    const searchParams = useSearchParams();
-    const ADMIN_SECRET = searchParams.get("admin-secret") || "";
-
-    const { register, handleSubmit, formState: { errors }, reset } = useForm({
-        resolver: yupResolver(schema),
-    });
-    const router = useRouter();
-
-    //  handle success
-    useEffect(() => {
-        if (isSuccess) {
-            toast.success("Signed In Successfully");
-            reset();
-            router.push("/");
-        }
-    }, [isSuccess]);
-
-
-    
-
-
-    // handle sign In
-    const adminSignInHandler = (data: any) => {
-        signIn(data)
-    };
-
-    if (!ADMIN_SECRET || ADMIN_SECRET !== process.env.NEXT_PUBLIC_ADMIN_SECRET) {
-        return (
-            <div className="flex items-center justify-center h-screen flex-col">
-                <Image src={warning} className="w-[40%] md:w-[35%] lg:w-[22%] mb-5" alt="smiley-face" />
-                <h1 className='text-white font-bold text-xl md:text-3xl xl:text-4xl mb-10'> Your Are Not Allowed To Access this Page <span className="text-red-500">!</span>  </h1>
-                <Link href="/" className='px-5 py-2 border-2 border-red-500 text-white font-medium'> Back to Home </Link>
-            </div>
-        )
-    }
-
-    return (
-        <div className="font-montserrat px-5 md:px-10 py-20 md:py-26 flex h-screen items-center justify-center">
-            <div className="rounded-md w-full sm:w-[70%] md:-w-[60%] lg:w-[40%] xl:w-[35%]  p-4 md:p-6 lg:p-8 xl:p-10" style={{
-                boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px"
-            }}>
-
-                <div className={`${isLoading ? "opacity-40 pointer-events-none" : ""} font-helvatica flex items-center justify-center pr-12 -mt-6 mb-5`}>
-                    <Link href="/">
-                        <h1 className="first-letter:text-6xl relative text-white"> S <span className="absolute top-3 text-2xl"> iddik </span>
-                            <span className="absolute bottom-2 left-9 text-[9px] text-primary-yellow"> RESTAURANT </span>
-                        </h1>
-                    </Link>
-                </div>
-
-                <h3 className="text-white mb-4 text-center text-xl md:text-2xl font-bold"> {isLoading ? <Image className="flex mx-auto items-center justify-center" src={loadingGear} alt="loading-gear" width={60} height={60} /> : "Admin Sign In"} </h3>
-                {/*  Signin form */}
-                <div className={`${isLoading ? "opacity-40 pointer-events-none" : ""}`}>
-
-                    <form className='' onSubmit={handleSubmit(adminSignInHandler)} >
-
-                        <Input data={{ name: "Email", type: 'email', placeholder: `Enter your Email`, error: errors.email?.message, hookFormRegister: register("email") }} />
-                        <Input data={{ name: "Password", type: 'password', placeholder: `Enter your Password`, error: errors.password?.message, hookFormRegister: register("password") }} />
-                        <small className="-mt-4 mb-5 text-white block">
-                            Forgot password ?
-                            <Link href="/reset-password" className="font-bold hover:border-b"> Reset </Link>
-                        </small>
-                        <button type="submit" disabled={isLoading} className="px-4 py-2 rounded border-2 border-primary-yellow text-white uppercase"> sign in  </button>
-                    </form>
-
-                    {/*  sign up  with other options */}
-                    <div className="text-center mt-5">
-                        <p className="text-white mb-3 text-lg"> Or Continue with </p>
-                        <SocialMediaAuth disabled={isLoading} />
-                        <p className="mt-8 text-white text-base">
-                            Don&apos;t have an account ?
-                            <Link href="/signup" className="font-bold hover:border-b"> Sign Up </Link>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+  return <AdminSignIn />
 }
 
-export default AdminSignInPage
+export default AdminSignInPage;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,122 +1,12 @@
-"use client";
-import loadingGear from "@/assets/images/loading-gear.gif";
-import SocialMediaAuth from "@/components/SocialMediaAuth/SocialMediaAuth";
-import Input from "@/components/shared/Input/Input";
-import { useSignupMutation } from "@/redux/features/signup/signupApi";
-import uploadImage from "@/utils/uploadImage";
-import { yupResolver } from "@hookform/resolvers/yup";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import toast from 'react-hot-toast';
-import * as yup from "yup";
-import { Metadata } from "next";
-
-const schema = yup.object().shape({
-  username: yup.string().max(45).required(),
-  email: yup.string().email().required(),
-  password: yup.string().min(10).max(30).required(),
-  picture: yup.mixed()
-    .test('required', "You need to provide an image", (value: any) => {
-      return value && value.length
-    })
-    .test("fileSize", "The file is too large", (value: any, context) => {
-      return value && value[0] && value[0].size <= 6 * 1024 * 1024 // accepted file size 6 MB;
-    })
-    .test("type", "Only the following formats are accepted: .png, .jpeg, .jpg", function (value: any) {
-      return value && value[0] && ["image/jpeg", "image/png", "image/jpg"].includes(value[0].type);
-    }),
-});
+import SignUp from "@/components/SignUp/SignUp"
+import { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: 'Sign Up - Siddik Restaurant',
 }
 
-
 const SignUpPage = () => {
-  // rtk sign up Hook
-  const [signUp, { isSuccess, data, isLoading, isError, error }] = useSignupMutation();
-  //console.log("data:", data)
-
-  // hook form hooks
-  const { register, handleSubmit, formState: { errors }, reset } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const [isUploadingImg, setIsUploadingImg] = useState(false);
-  const router = useRouter();
-  //console.log("error:", error)
-
-// handle success 
-  useEffect(() => {
-    if (isSuccess) {
-      toast.success("Signed Up Successfully");
-      reset();
-      router.push("/signin");
-    }
-  }, [isSuccess]);
-
-  // handle sign up
-  const SignUpHandler = async (data: any) => {
-    setIsUploadingImg(true);
-    try {
-      const url = await uploadImage(data.picture[0]);
-      await signUp({ ...data, picture: url });
-    } catch (error: any) {
-      console.log("error:", error)
-    } finally {
-      setIsUploadingImg(false)
-    }
-  };
-
-
-  return (
-    <>
-
-      <div className="font-montserrat px-5 md:px-10 py-18 md:py-26 flex h-screen items-center justify-center">
-        <div className="rounded-md w-full sm:w-[70%] md:-w-[60%] lg:w-[40%] xl:w-[35%]  p-4 md:p-6 lg:p-8 xl:p-10" style={{
-          boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px"
-        }}>
-
-          <div className={`${(isLoading || isUploadingImg) ? "opacity-40 pointer-events-none" : ""} font-helvatica flex items-center justify-center pr-12 -mt-6 mb-5`}>
-            <Link href="/">
-              <h1 className="first-letter:text-6xl relative text-white"> S <span className="absolute top-3 text-2xl"> iddik </span>
-                <span className="absolute bottom-2 left-9 text-[9px] text-primary-yellow"> RESTAURANT </span>
-              </h1>
-            </Link>
-          </div>
-
-          <h3 className="text-white mb-4 text-center text-xl md:text-2x font-bold"> {(isLoading || isUploadingImg) ? <Image className="flex mx-auto items-center justify-center" src={loadingGear} alt="loading-gear" width={60} height={60} /> : " Sign Up"} </h3>
-          {/*  Signin form */}
-          <div className={`${(isLoading || isUploadingImg) ? "opacity-40 pointer-events-none" : ""}`}>
-
-            <form onSubmit={handleSubmit(SignUpHandler)} >
-
-              <Input data={{ name: "Name", type: 'text', placeholder: `Enter your Full Name`, error: errors.username?.message, hookFormRegister: register("username") }} />
-              <Input data={{ name: "Email", type: 'email', placeholder: `Enter your Email`, error: errors.email?.message, hookFormRegister: register("email") }} />
-              <Input data={{ name: "Password", type: 'password', placeholder: `Enter your Password`, error: errors.password?.message, hookFormRegister: register("password") }} />
-
-              <Input data={{ name: "Picture", type: 'file', placeholder: `Upload your picture`, error: errors.picture?.message, hookFormRegister: register("picture") }} />
-
-              <button disabled={(isLoading || isUploadingImg)} type="submit" className="px-4 py-2 rounded border-2 border-primary-yellow text-white uppercase"> sign up </button>
-            </form>
-
-            {/*  sign up  with other options */}
-            <div className="text-center mt-5">
-              <p className="text-white mb-3 text-lg"> Or Continue with </p>
-              <SocialMediaAuth disabled={(isLoading || isUploadingImg)} />
-              <p className="mt-8 text-white text-base">
-                Already have an account ?
-                <Link href="/signin" className="font-bold hover:border-b"> Sign In </Link>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+  return <SignUp />
 }
 
 export default SignUpPage
